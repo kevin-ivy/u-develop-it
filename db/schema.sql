cb54907e85d581aafc2cfe40247306a863c08456
kIvy1189 <kevindivy@gmail.com>
2020-06-04 19:54:14 -0500

    Added voteRoutes and set up functionality to tally votes

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 
 CREATE TABLE candidates (
     id INTEGER PRIMARY KEY,
@@ -23,4 +24,14 @@ CREATE TABLE voters (
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE votes (
+    id integer primary key,
+    voter_id integer unsigned not null,
+    candidate_id integer unsigned not null,
+    created_at datetime default current_timestamp,
+    constraint uc_voter unique (voter_id),
+    constraint fk_voter foreign key (voter_id) references voters(id) on delete cascade,
+    constraint fk_candidate foreign key (candidate_id) references candidates(id) on delete cascade
 );
